6e2ff21b5c8b7c623adab06745321887a3ec5a12
Saif <mujawarmsaif@gmail.com>
2020-04-30 13:35:42 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -3,26 +3,33 @@ const dec = document.querySelector('.decrement');
 const reset = document.querySelector('.reset');
 const value = document.querySelector('.count');
 var count = 0;
+
 inc.addEventListener('click', () => {
 	// var t = e.target;
 	count += 1;
 	value.innerHTML = count;
+	increment();
 	store();
 	console.log(count);
 });
 
 dec.addEventListener('click', () => {
-	if (count > 0) {
-		count -= 1;
-		value.innerHTML = count;
-		} else {
-			value.innerHTML = count = 0;
+	var n = localStorage.getItem('count');
+	if (n > 0) {
+		n -= 1;
+		localStorage.setItem('count', n);
+	} else {
+		value.innerHTML = count = 0;
 	}
+	document.getElementById('result').innerHTML = n;
+
+	// decrement();
 	store();
 });
 
 reset.addEventListener('click', () => {
 	value.innerHTML = count = 0;
+	resetCounter();
 	store();
 });
 
@@ -39,3 +46,31 @@ function getValue() {
 	}
 }
 getValue();
+
+// function store() {
+// 	window.localStorage.mycounter = value.innerHTML;
+// }
+
+function increment() {
+	var n = localStorage.getItem('count');
+	if (n === 'null') {
+		n = n;
+	}
+	n++;
+	localStorage.setItem('count', n);
+
+	document.getElementById('result').innerHTML = n;
+	console.log(n);
+}
+
+function decrement() {}
+function resetCounter() {
+	var n = localStorage.getItem('count');
+	if (n === 1) {
+		n = 0;
+	}
+	n = 0;
+	localStorage.setItem('count', n);
+
+	document.getElementById('result').innerHTML = n;
+}
